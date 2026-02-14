5162e67682480451e3f88cde5469aaf78b5687a6
yangzinuo <yangzinuo@caohua.com>
2026-02-14 11:19:57 +0800

    Fix/修复应用与通知栏重叠问题

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,8 +2,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.zane.hydratemate',
-  appName: 'HydrateMate',
-  webDir: 'dist'
+  appName: '喝了么',
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: false,
+      backgroundColor: "#051405",
+      androidSplashResourceName: "splash",
+      showSpinner: false,
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
